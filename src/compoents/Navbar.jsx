3f27a3c75300ac9ed0b8a2140924f868bcad793f
tuseefahmed786 <tuseefahmed8880@gmail.com>
2025-02-25 00:29:57 +0500

    we have added new thing and responsive design

--- src/compoents/Navbar.jsx
+++ src/compoents/Navbar.jsx
@@ -7,9 +7,9 @@ function Navbar() {
   return (
     <>
       <div className='flex justify-around items-center px-5 py-3'>
-        <img src={logo} alt="" />
+        <img className='w-24 md:w-48' src={logo} alt="" />
         <div className='flex gap-5 justify-end items-center w-1/5'>
-       <p>Menu</p>
+       {/* <p className=''>Menu</p> */}
         <svg className='w-8' aria-hidden="true" focusable="false" data-prefix="fal" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M442 114H6a6 6 0 0 1-6-6V84a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6z"></path></svg>
         </div>
         <div className='flex gap-5'>
